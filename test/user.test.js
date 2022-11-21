2ac40d9750f5a18b4175fca3f8c23783f51dc937
carlosouzapsi <carlosouzapsi@gmail.com>
2022-11-20 23:10:30 -0300

    Updating classes node api

--- test/user.test.js
+++ test/user.test.js
@@ -12,5 +12,11 @@ test("Deve listar todos os usuários", () => {
 });
 
 test("Deve inserir o usuário com sucesso", () => {
-  return request(app).post();
+  return request(app)
+    .post("/users")
+    .send({ name: "Walter Mitty", mail: "walter@mail.com" })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe("Walter Mitty");
+    });
 });
